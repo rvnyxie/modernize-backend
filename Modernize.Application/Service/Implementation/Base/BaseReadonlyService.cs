94069797269a34a5ce984197f4f044f440f07f82
unknown <thanhnam.awa@gmail.com>
2024-12-11 18:26:40 +0700

    feat: add applicaiton service implementation

---
+++ Modernize.Application/Service/Implementation/Base/BaseReadonlyService.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Modernize.Domain;
+
+namespace Modernize.Application
+{
+    public class BaseReadonlyService<TEntity, TDtoEntity, TId> : IBaseReadonlyService<TDtoEntity, TId>
+    {
+        protected readonly IBaseReadonlyRepository<TEntity, TId> BaseReadonlyRepository;
+        protected readonly IMapper Mapper;
+
+        public BaseReadonlyService(IBaseReadonlyRepository<TEntity, TId> baseReadonlyRepository, IMapper mapper)
+        {
+            BaseReadonlyRepository = baseReadonlyRepository;
+            Mapper = mapper;
+        }
+
+        public async Task<IEnumerable<TDtoEntity>> GetAllAsync()
+        {
+            var entities = await BaseReadonlyRepository.GetAllAsync();
+
+            var dtoEntities = entities.Select(entity => Mapper.Map(TEntity, TDtoEntiy)).ToList();
+
+            return dtoEntities;
+        }
+
+        public Task<TDtoEntity> GetByIdAsync(TId id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
