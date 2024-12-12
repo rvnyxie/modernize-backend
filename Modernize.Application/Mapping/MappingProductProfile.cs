9619c93d97c5f346c719c8b6ca74db1c15f51367
unknown <thanhnam.awa@gmail.com>
2024-12-12 15:29:00 +0700

    refactor: organize product mapping profile

--- Modernize.Application/Mapping/MappingProductProfile.cs
+++ Modernize.Application/Mapping/MappingProductProfile.cs
@@ -3,20 +3,25 @@ using Modernize.Domain;
 
 namespace Modernize.Application
 {
+    /// <summary>
+    /// Product mapping profile used for AutoMapper
+    /// </summary>
     public class MappingProductProfile : Profile
     {
         public MappingProductProfile()
         {
+            // Query, Command
             CreateMap<CreateProductCommand, ProductCreationDto>();
             CreateMap<UpdateProductCommand, ProductUpdateDto>();
-            CreateMap<Product, ProductDto>();
-            CreateMap<ProductCreationDto, ProductDto>()
-                .ForMember(dest => dest.Id, opt => opt.Ignore());
-            CreateMap<ProductUpdateDto, ProductDto>();
+
+            // To Entity
             CreateMap<ProductCreationDto, Product>()
                 .ForMember(dest => dest.Group, opt => opt.Ignore())
                 .ForMember(dest => dest.Id, opt => opt.Ignore());
             CreateMap<ProductUpdateDto, Product>();
+
+            // To DTO
+            CreateMap<Product, ProductDto>();
         }
     }
 }
